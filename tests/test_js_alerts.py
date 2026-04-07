6bf91c637bf037327c1ae4cfcb4156e9f3ac2948
Colin Koo <colin_k95@hotmail.com>
2026-04-06 18:43:33 -0700

    fixed logic for handle_prompt test, modularized tests, creating separate methods for each test to eliminate loops and to self-contain test cases

--- tests/test_js_alerts.py
+++ tests/test_js_alerts.py
@@ -13,19 +13,29 @@ def test_js_alerts(page: Page, count: int) -> None:
     jsalerts.handle_alert(count)
     expect(jsalerts.page.locator("#result")).to_have_text("You successfully clicked an alert")
     
-    for i in range(count):
-        random_option = random.choice([True, False])
-        jsalerts.handle_confirm(random_option)
-        if random_option:
-            expect(jsalerts.page.locator("#result")).to_have_text("You clicked: Ok")  
-        else: 
-            expect(jsalerts.page.locator("#result")).to_have_text("You clicked: Cancel")
+    # for input_text, option in test_prompts:
+    #     jsalerts.handle_prompt(input_text, option)
+    #     prev_text = jsalerts.page.locator("#result").inner_text()
+    #     if option:
+    #         expect(jsalerts.page.locator("#result")).to_have_text(f"You entered: {input_text}")
+    #     else:
+    #         expect(jsalerts.page.locator("#result")).to_have_text(f"{prev_text}")
 
-    test_prompts = [("null", True), ("", True), ("Playwright", True), ("null", False), ("テスト", True), ("テストケース9990000", True)]
-    for input_text, option in test_prompts:
-        jsalerts.handle_prompt(input_text, option)
-        prev_text = jsalerts.page.locator("#result").inner_text()
-        if option:
-            expect(jsalerts.page.locator("#result")).to_have_text(f"You entered: {input_text}")
-        else:
-            expect(jsalerts.page.locator("#result")).to_have_text(f"{prev_text}")
+@pytest.mark.parametrize("choice", [True, False])
+def test_js_confirm_dismiss(page: Page, choice: bool) -> None:
+    jsalerts = JSAlertsPage(page)
+    jsalerts.open()
+    jsalerts.handle_confirm(choice)
+    expected_text = "You clicked: Ok" if choice else "You clicked: Cancel"
+    expect(jsalerts.page.locator("#result")).to_have_text(expected_text)
+
+@pytest.mark.parametrize("test_prompts", [("null", True), ("", True), ("Playwright", True), ("null", False), ("テスト", True), ("テストケース9990000", True)])
+def test_js_prompt(page: Page, test_prompts: tuple[str, bool]) -> None:
+    jsalerts = JSAlertsPage(page)
+    jsalerts.open()
+    input_text, option = test_prompts
+    jsalerts.handle_prompt(input_text, option)
+    if option:
+        expect(jsalerts.page.locator("#result")).to_have_text(f"You entered: {input_text}")
+    else:
+        expect(jsalerts.page.locator("#result")).to_have_text(f"You entered: null")
