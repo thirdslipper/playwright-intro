6b6d01c1215afd5934c309efe546d216b6b8b76c
Colin Koo <colin_k95@hotmail.com>
2026-04-24 01:28:32 -0700

    Added tests for verifying text on the page behind shadowdom.

--- pages/shadowdom_page.py
+++ pages/shadowdom_page.py
@@ -11,7 +11,8 @@ class ShadowdomPage(BasePage):
         """ This calls the BasePage's __init__ to set up self.page and self.base_url"""
         super().__init__(page) 
         #grab all the dropdowns on the page and store them into var dropdowns
-        # self.shadowdom = page.get_by_role("combobox")
+        self.shadow_host = page.locator("my-paragraph")
+        self.ul_slot = page.get_by_role("list")
         logger.info("DropdownPage initialized with shadowdom elements.")
         # self.dropdown_elements = dropdown.get_by_role("option")
         
@@ -21,5 +22,18 @@ class ShadowdomPage(BasePage):
         self.navigate_to(PAGE_PATH)
         logger.info(f"Navigating to page {PAGE_PATH}")
         # wait for the <select> element to be present, then read its options
-        # self.dropdown_elements.first.wait_for()
-        
+        self.shadow_host.first.wait_for()
+        self.ul_slot.first.wait_for()
+    
+    def get_span_slot_text(self) -> str:
+        span_var = self.shadow_host.locator('span[slot="my-text"]').first #.inner_text()
+        logger.debug(f"span_slot text is: {span_var.inner_text()}")
+        return span_var.inner_text()
+    
+    def get_list_text(self) -> list:
+        list_res = []
+        list_var = self.ul_slot.get_by_role("listitem")
+        for i, item in enumerate(list_var.all()):
+            logger.debug(f"item : {i} has contents: {item.inner_text()}")
+            list_res.append(item.inner_text())
+        return list_res
--- tests/test_shadowdom.py
+++ tests/test_shadowdom.py
@@ -2,10 +2,20 @@ from playwright.sync_api import expect, Page
 import pytest #Playwright, sync_playwright, 
 from pages.shadowdom_page import ShadowdomPage
 
-# @pytest.mark.parametrize("target", [[("B", "A"), ("A", "B"), ("B", "A")]])
+test_text = "Let's have some different text!"
+test_text_list = "In a list!"
+
+
 def test_shadowdom(page: Page) -> None:
-    """Intent: 
+    """Intent: Verify elements are present with shadowdom elements on the page.
     """
     shadowdom = ShadowdomPage(page)
     shadowdom.open()
-    # assert shadowdom.get_header_text() == "Simple template"
+    assert(shadowdom.get_span_slot_text() == test_text)
+
+def test_span_slot(page: Page) -> None:
+    shadowdom = ShadowdomPage(page)
+    shadowdom.open()
+    dom_list = shadowdom.get_list_text()
+    assert(dom_list[0] == test_text)
+    assert(dom_list[1] == test_text_list)
